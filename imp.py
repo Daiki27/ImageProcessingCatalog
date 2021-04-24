ad28afa8bb81c1543d7a7787f120808c9f9e95ae
DaikiHiguchi <daikiit27@gmail.com>
2021-04-25 00:34:22 +0900

    refactoring

--- imp.py
+++ imp.py
@@ -1,6 +1,10 @@
 
 import cv2
+import os
 
 srcImg = cv2.imread("italy.jpg")
 dstImg = cv2.xphoto.oilPainting(srcImg, 3, 1)
-cv2.imwrite("italy-oilPainted.jpg",dstImg)
+
+os.makedirs('result', exist_ok=True)
+
+cv2.imwrite("result/italy-oilPainted.jpg",dstImg)
